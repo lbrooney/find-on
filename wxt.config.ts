6948e1a9847382f703b2a0ea1a9d9b0d361f4310
Liam Rooney <liam@roon.dev>
2025-06-25 21:23:52 -0700

    limit host permissions to just HN and reddit API

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,18 +4,23 @@ import { defineConfig, type WxtViteConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	imports: false,
-	srcDir: "src",
-	modules: ["@wxt-dev/module-solid"],
 	manifest: {
-		name: "Find On - Reddit + Hacker News",
-		permissions: ["activeTab", "tabs", "storage"],
-		host_permissions: ["http://*/*", "https://*/*"],
 		browser_specific_settings: {
 			gecko: {
 				id: "{fa784cb5-9feb-4063-a712-43c1ce2d4ba5}",
 			},
 		},
+		host_permissions: [
+			"https://api.reddit.com/*",
+			"https://reddit.com/api/*",
+			"https://www.reddit.com/api/*",
+			"https://hn.algolia.com/api/v1/search/*",
+		],
+		name: "Find On - Reddit + Hacker News",
+		permissions: ["activeTab", "tabs", "storage"],
 	},
+	modules: ["@wxt-dev/module-solid"],
+	srcDir: "src",
 	vite: () =>
 		({
 			plugins: [tailwindcss()],
